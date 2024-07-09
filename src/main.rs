433c61b95b5e25e658e127dfa9513d6c5b95338f
Joshua Small <joshuasmall@microsoft.com>
2024-07-09 18:30:49 +0100

    clean up old code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use chrono::{Duration, NaiveDate};
+use anyhow::{anyhow, Error};
+use chrono::{NaiveDate};
 use std::collections::BTreeMap;
 use std::io::{self, Write};
-use anyhow::{anyhow, Error};
 
 #[derive(Clone, Debug)]
 struct Loan {
@@ -11,55 +11,59 @@ struct Loan {
     loan_currency: String,
     base_interest_rate: f64,
     margin: f64,
-    total_interest: f64,
-    // This could be a vector but we may want to access daily information by date in the future.
-    // BTreeMap is used as it is sorted by key and efficient for lookups.
-    daily_information: BTreeMap<NaiveDate, Daily_Information>,
-}
-
-#[derive(Clone, Copy, Debug)]
-#[allow(non_camel_case_types)]
-struct Daily_Information {
-    day_interest: f64,
-    day_interest_no_margin: f64,
-    days_elapsed: i64,
+    // figure out how to add id here and if it is the right thing to do
+    // id: u32,
+    // not including total_interest in the struct as it is calculated and not set by the user, meaning if another field is changed it could get out of sync
+    // total_interest: Option<f64>,
 }
 
 /// Create a method new() for the Loan struct that takes in no values and returns a Loan with default values.
 impl Loan {
-    fn new() -> Self {
+    fn new(
+        start_date: NaiveDate,
+        end_date: NaiveDate,
+        loan_amount: f64,
+        loan_currency: String,
+        base_interest_rate: f64,
+        margin: f64,
+        // id: u32,
+    ) -> Self {
         Loan {
-            loan_amount: 1000.0,
-            start_date: NaiveDate::from_ymd_opt(2020, 1, 1).unwrap(),
-            end_date: NaiveDate::from_ymd_opt(2020, 1, 5).unwrap(),
-            loan_currency: "USD".to_string(),
-            base_interest_rate: 0.05,
-            margin: 0.01,
-            total_interest: 0.0,
-            daily_information: BTreeMap::new(),
+            start_date,
+            end_date,
+            loan_amount,
+            loan_currency,
+            base_interest_rate,
+            margin,
+            // id,
         }
     }
-    fn calculate_interest(&mut self) -> () {
-        let days = self.end_date.signed_duration_since(self.start_date).num_days();
+    fn calculate_interest(&self) -> f64 {
+        let days = self
+            .end_date
+            .signed_duration_since(self.start_date)
+            .num_days();
         let total_interest_rate = self.base_interest_rate + self.margin;
-        let daily_interest_rate_no_margin = self.base_interest_rate / 365.0;
         let daily_interest_rate = total_interest_rate / 365.0;
 
-        // This could be done more concisely but having it structured like this allows the interest to be changed to a more complex type in the future.
-        for day in 1..days+1 {
-            let current_date = self.start_date + Duration::days(day);
-            let daily_interest_amount_no_margin = self.loan_amount * daily_interest_rate_no_margin;
-            let daily_interest_amount = self.loan_amount * daily_interest_rate;
-            let daily_information = Daily_Information {
-                day_interest: daily_interest_amount,
-                day_interest_no_margin: daily_interest_amount_no_margin,
-                days_elapsed: day,
-            };
-            self.daily_information.insert(current_date, daily_information);
-        }
-        let total_interest: f64 = self.loan_amount * daily_interest_rate * days as f64;
-        self.total_interest = total_interest;
+        self.loan_amount * daily_interest_rate * days as f64
     }
+    fn display(&self) {
+        println!("Loan Information");
+        println!("----------------");
+        println!("Start Date: {}", self.start_date);
+        println!("End Date: {}", self.end_date);
+        println!("Loan Amount: {} {}", self.loan_amount, self.loan_currency);
+        println!("Loan Currency: {}", self.loan_currency);
+        println!("Base Interest Rate: {}%", self.base_interest_rate * 100.0);
+        println!("Margin: {}%", self.margin * 100.0);
+        println!(
+            "Total Interest: {:.2} {}",
+            self.calculate_interest(),
+            self.loan_currency
+        );
+    }
+        
 }
 
 #[derive(Debug)]
@@ -94,9 +98,17 @@ impl LoanCalculator {
             Err(anyhow!("Loan with ID {} not found.\n", loan_id))
         }
     }
+     
+    fn display(&self) {
+        println!("Loan IDs:");
+        println!("----------------");
+        for (loan_id, _) in self.loans.iter() {
+            println!("Loan ID: {}", loan_id);
+        }
+    }
 }
 
-fn main() -> Result<(), Error>{
+fn main() -> Result<(), Error> {
     println!("Loan Interest Calculator");
 
     let mut calculator = LoanCalculator::new();
@@ -107,31 +119,26 @@ fn main() -> Result<(), Error>{
         println!("2. Update Loan");
         println!("3. Show Loan Information");
         println!("4. Show All Loans");
-        println!("5. Exit");
+        println!("5. Create Mock Data");
+        println!("6. Exit");
         print!("Please enter your choice: ");
-        io::stdout().flush().unwrap();
 
-        let mut choice = String::new();
-        io::stdin().read_line(&mut choice).unwrap();
-
+        let choice = get_input();
+        // print 4 new lines
+        println!("\n\n\n\n");
         // quick error handling for non integer input
         let choice: u32 = choice.trim().parse::<u32>().or::<u32>(Ok(999)).unwrap();
         let result = match choice {
-            1 => {
-                add_loan(&mut calculator)
-            }
-            2 => {
-                update_loan(&mut calculator)   
-            }
-            3 => {
-                show_loan_information(&mut calculator)
-            }
-            4 => {
-                show_all_loans(&mut calculator)
-            }
+            1 => add_loan(&mut calculator),
+            2 => update_loan(&mut calculator),
+            3 => show_loan_information(&mut calculator),
+            4 => show_all_loans(&mut calculator),
             5 => {
+                create_mock_data(&mut calculator)
+            }
+            6 => {
                 println!("Exiting...");
-                break
+                break;
             }
             _ => {
                 println!("\nInvalid choice! Please enter an integer from 1-5.");
@@ -145,27 +152,64 @@ fn main() -> Result<(), Error>{
     Ok(())
 }
 
+fn create_mock_data(calculator: &mut LoanCalculator) -> Result<(), Error> {
+
+    let loan1 = Loan::new(
+        NaiveDate::from_ymd_opt(2020, 1, 1).unwrap(),
+        NaiveDate::from_ymd_opt(2020, 1, 31).unwrap(),
+        1000.0,
+        "USD".to_string(),
+        0.05,
+        0.01,
+    );
+    let loan2 = Loan::new(
+        NaiveDate::from_ymd_opt(2020, 1, 1).unwrap(),
+        NaiveDate::from_ymd_opt(2021, 1, 1).unwrap(),
+        2000.0,
+        "USD".to_string(),
+        0.055,
+        0.02,
+    );
+
+    let loan3 = Loan::new(
+        NaiveDate::from_ymd_opt(2020, 4, 3).unwrap(),
+        NaiveDate::from_ymd_opt(2020, 7, 21).unwrap(),
+        10000.0,
+        "USD".to_string(),
+        0.10,
+        0.01,
+    );
+
+    calculator.add_loan(loan1);
+    calculator.add_loan(loan2);
+    calculator.add_loan(loan3);
+
+    show_all_loans(calculator)
+}
+
 fn show_all_loans(calculator: &mut LoanCalculator) -> Result<(), Error> {
     println!("All Loans:");
-    for (loan_id, loan) in calculator.loans.iter() {
-        println!("Loan ID: {}", loan_id);
-        println!("{:#?}\n", loan);
-    }
+    // println!("{:?}", calculator);
+    calculator.display();
+    println!("\n\n\n\n");
     Ok(())
+}
+
+fn get_input() -> String {
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
 }
 
 fn update_loan(calculator: &mut LoanCalculator) -> Result<(), Error> {
     print!("Enter the Loan ID to update: ");
-    io::stdout().flush().unwrap();
-    let mut loan_id_input = String::new();
-    io::stdin().read_line(&mut loan_id_input).unwrap();
+    let loan_id_input = get_input();
+
     let loan_id: u32 = loan_id_input.trim().parse()?;
     if let Some(mut loan) = calculator.loans.get(&loan_id).cloned() {
-        // reset total_interest to 0 so it can be recalculated
-        loan.total_interest = 0.0;
-        // reset daily_information to empty so it can be recalculated
-        loan.daily_information = BTreeMap::new();
-        loan = update_loan_parameters(loan)?;
+        // need to change this
+        loan = create_loan()?;
         calculator.update_loan(loan_id, loan)?;
         let new_loan = calculator.loans.get_mut(&loan_id).unwrap();
         new_loan.calculate_interest();
@@ -175,20 +219,27 @@ fn update_loan(calculator: &mut LoanCalculator) -> Result<(), Error> {
     }
 }
 
-fn show_loan_information(calculator: &mut LoanCalculator) -> Result<(), Error>{
-    print!("Enter the Loan ID: ");
-    io::stdout().flush().unwrap();
-    let mut loan_id_input = String::new();
-    io::stdin().read_line(&mut loan_id_input).unwrap();
-    let loan_id: u32 = loan_id_input.trim().parse()?;
-
-    let loan = calculator.loans.get_mut(&loan_id).ok_or(anyhow!("Loan with ID {} not found.\n", loan_id))?;
-    print_interest_results(loan.clone());
+fn show_loan_with_id(calculator: &LoanCalculator, loan_id: &u32) -> Result<(), Error> {
+    let loan = calculator
+        .loans
+        .get(&loan_id)
+        .ok_or(anyhow!("Loan with ID {} not found.\n", loan_id))?;
+    loan.display();
     Ok(())
 }
 
-fn add_loan(calculator: &mut LoanCalculator) -> Result<(), Error>{
-    let loan = update_loan_parameters(Loan::new())?;
+fn show_loan_information(calculator: &mut LoanCalculator) -> Result<(), Error> {
+    print!("Enter the Loan ID: ");
+    let loan_id_input = get_input();
+    // improve error message below, "could not get loan ID form input"
+    let loan_id: u32 = loan_id_input.trim().parse()?;
+
+    show_loan_with_id(calculator, &loan_id)?;
+    Ok(())
+}
+
+fn add_loan(calculator: &mut LoanCalculator) -> Result<(), Error> {
+    let loan = create_loan()?;
     let loan_id = calculator.add_loan(loan);
     let added_loan = calculator.loans.get_mut(&loan_id).unwrap();
     println!("Loan added with ID: {}\n", loan_id);
@@ -196,57 +247,35 @@ fn add_loan(calculator: &mut LoanCalculator) -> Result<(), Error>{
     Ok(())
 }
 
-fn print_interest_results(loan: Loan) {
-    println!("Loan Interest Calculation Results");
-    println!("--------------------------------");
-    // printing could be prettier but this is just a demo
-    // it is more important that the calculations are correct and we do not round too early
-    println!("{:#?}\n", loan);
-}
-
-fn update_loan_parameters(mut loan: Loan) -> Result<Loan, Error> {
-    println!("Update Loan Parameters");
-    println!("----------------------");
+/// Can improve this function to allow a single input to fail and retry not all inputs
+fn create_loan() -> Result<Loan, Error> {
 
     print!("Start Date (YYYY-MM-DD): ");
-    io::stdout().flush().unwrap();
-    let mut start_date = String::new();
-    io::stdin().read_line(&mut start_date).unwrap();
-    loan.start_date = NaiveDate::parse_from_str(&start_date.trim(), "%Y-%m-%d")?;
+    let start_date = NaiveDate::parse_from_str(get_input().as_str(), "%Y-%m-%d")?;
 
     print!("End Date (YYYY-MM-DD): ");
-    io::stdout().flush().unwrap();
-    let mut end_date = String::new();
-    io::stdin().read_line(&mut end_date).unwrap();
-    loan.end_date = NaiveDate::parse_from_str(&end_date.trim(), "%Y-%m-%d")?;
+    let end_date = NaiveDate::parse_from_str(get_input().as_str(), "%Y-%m-%d")?;
 
     print!("Loan Amount: ");
-    io::stdout().flush().unwrap();
-    let mut loan_amount = String::new();
-    io::stdin().read_line(&mut loan_amount).unwrap();
-    loan.loan_amount = loan_amount.trim().parse()?;
+    let loan_amount = get_input().trim().parse()?;
 
     print!("Loan Currency: ");
-    io::stdout().flush().unwrap();
-    let mut loan_currency = String::new();
-    io::stdin().read_line(&mut loan_currency).unwrap();
-    loan.loan_currency = loan_currency.trim().to_string();
+    let loan_currency = get_input().trim().parse()?;
 
     print!("Base Interest Rate (%): ");
-    io::stdout().flush().unwrap();
-    let mut base_interest_rate = String::new();
-    io::stdin().read_line(&mut base_interest_rate).unwrap();
     // divide by 100 to convert to %
-    loan.base_interest_rate = base_interest_rate.trim().parse::<f64>()?/100.0;
+    let base_interest_rate = get_input().trim().parse::<f64>()? / 100.0;
 
     print!("Margin (%): ");
-    io::stdout().flush().unwrap();
-    let mut margin = String::new();
-    io::stdin().read_line(&mut margin).unwrap();
     // divide by 100 to convert to %
-    loan.margin = margin.trim().parse::<f64>()?/100.0;
+    let margin = get_input().trim().parse::<f64>()? / 100.0;
 
-    println!("Loan parameters updated successfully!\n");
-
-    Ok(loan)
+    Ok(Loan::new(
+        start_date,
+        end_date,
+        loan_amount,
+        loan_currency,
+        base_interest_rate,
+        margin,
+    ))
 }
